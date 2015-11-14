8e5b38842e0e1ef16bf3f7e3188f1d9842e8ab75
Steve Geffner <steve.geffner.phd@gmail.com>
2015-11-14 14:58:50 -0800

    Updated Adafruit color sensor demo to show how to control the LED manually with a digital port on the core device interface.
    Former-commit-id: 60a5a209cf1fca31e566728fe5e8ba268770c76c
    Former-commit-id: 6b8f075df2d403f98889cea13596c03781f1d808

--- SwerveRoboticsLibrary/src/main/java/org/swerverobotics/library/examples/SynchAdaColorSensorDemo.java
+++ SwerveRoboticsLibrary/src/main/java/org/swerverobotics/library/examples/SynchAdaColorSensorDemo.java
@@ -2,6 +2,8 @@ package org.swerverobotics.library.examples;
 
 import com.qualcomm.hardware.AdafruitI2cColorSensor;
 import com.qualcomm.robotcore.hardware.ColorSensor;
+import com.qualcomm.robotcore.hardware.DigitalChannel;
+import com.qualcomm.robotcore.hardware.DigitalChannelController;
 import com.qualcomm.robotcore.hardware.LED;
 
 import org.swerverobotics.library.SynchronousOpMode;
@@ -11,34 +13,52 @@ import org.swerverobotics.library.interfaces.TeleOp;
 
 /**
  * SynchLEDDemo is a short demo on using the Adafruit Color Sensor module.
- * It assumes that you have an Adafruit Color Sensor connected to a Core Device Interface
- * called "color". We don't really need to control the sensor's LED - it comes on by default.
+ * It assumes that:
+ * an Adafruit Color Sensor is connected to a Core Device Interface I2C port and called "color"
+ * the led for the AdaFruit is connected to a Core Device Interface digital port and called "led"
  */
-@TeleOp(name="AdaColorSensor", group="Swerve Examples")
+@TeleOp(name = "AdaColorSensor", group = "Swerve Examples")
 public class SynchAdaColorSensorDemo extends SynchronousOpMode {
 
     ColorSensor sensorRGB = null;
+    DigitalChannel sensorLED = null;
 
-    @Override public void main() throws InterruptedException {
+    final boolean LED_ON = true;
+    final boolean LED_OFF = false;
+
+    @Override
+    public void main() throws InterruptedException {
 
         // get a reference to our ColorSensor object.
         sensorRGB = hardwareMap.colorSensor.get("color");
+
+        // get a reference to the LED on the color sensor board
+        sensorLED = hardwareMap.digitalChannel.get("led");
+        sensorLED.setMode(DigitalChannelController.Mode.OUTPUT);
+        sensorLED.setState(LED_ON);
 
         // Set up our dashboard computations
         composeDashboard();
 
         // Wait until we're told to go
         waitForStart();
-               
+
         // Loop and update the dashboard
         while (this.opModeIsActive()) {
+
+            if (this.updateGamepads()) {
+                if (this.gamepad1.a) {
+                    //toggle LED state
+                    sensorLED.setState( !sensorLED.getState() );
+                }
+            }
 
             telemetry.update();
 
             idle();
         }
     }
-    
+
     void composeDashboard() {
         telemetry.addLine(
                 telemetry.item("loop count: ", new IFunc<Object>() {
@@ -49,13 +69,21 @@ public class SynchAdaColorSensorDemo extends SynchronousOpMode {
                 }));
         telemetry.addLine(
                 telemetry.item("color: ", new IFunc<Object>() {
-                            @Override
-                            public Object value() {
-                                //return led.getState(); //unlike DigitalChannel, leds don't let you read their state, so use the variable here instead.
-                                return "r: " + sensorRGB.red() + " g: " + sensorRGB.green() + " b: " + sensorRGB.blue();
-                            }
-                        })
+                    @Override
+                    public Object value() {
+                        return "r: " + sensorRGB.red() + " g: " + sensorRGB.green() + " b: " + sensorRGB.blue();
+                    }
+                })
         );
+        telemetry.addLine(
+                telemetry.item("led: ", new IFunc<Object>() {
+                    @Override
+                    public Object value() {
+                        return sensorLED.getState();
+                    }
+                })
+        );
+
     }
 
 }
