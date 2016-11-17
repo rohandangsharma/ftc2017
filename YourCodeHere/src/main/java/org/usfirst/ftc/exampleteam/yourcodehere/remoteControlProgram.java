b4b992d04461b992a4fcf984af7376668e8eb9c6
rohandangsharma <rohandangsharma@gmail.com>
2016-11-17 18:37:36 -0500

    Nothing New

--- YourCodeHere/src/main/java/org/usfirst/ftc/exampleteam/yourcodehere/remoteControlProgram.java
+++ YourCodeHere/src/main/java/org/usfirst/ftc/exampleteam/yourcodehere/remoteControlProgram.java
@@ -5,13 +5,10 @@
 //Run from the necessary package
 package org.usfirst.ftc.exampleteam.yourcodehere;
 
-
 //Import necessary items
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.Servo;
-import org.swerverobotics.library.SynchronousOpMode;
-
+import com.qualcomm.robotcore.hardware.*;
+import org.swerverobotics.library.*;
 
 @TeleOp(name="Remote Control Program") //Name the class
 public class remoteControlProgram extends SynchronousOpMode //CLASS START
@@ -26,18 +23,23 @@ public class remoteControlProgram extends SynchronousOpMode //CLASS START
     //DcMotor shooterLeft;
     //DcMotor shooterRight;
 
+<<<<<<< HEAD
 
 //    //Define Servo Motors
 //    Servo doorLeft;
 //    Servo doorRight;
-
+=======
+    //Define Servo Motors
+    Servo doorLeft;
+    Servo doorRight;
+>>>>>>> parent of d0a29b6... Patched
 
     //Define press counts
     //public int aPressCount = 0;
     //public int yPressCount = 0;
 
-
     //Define floats to be used as joystick and trigger inputs
+<<<<<<< HEAD
     float absDrivePower;
     float absShiftPower;
     float absTurnPower;
@@ -142,16 +144,48 @@ public class remoteControlProgram extends SynchronousOpMode //CLASS START
     //Create a custom function to count the number of times the button "a" is pressed
     //It toggles button a, so if it has been pressed an odd number of times, the motor will go forward
     //Otherwise, it will be off
+=======
+    float driveY;
+    float turnX;
+    float rightShift;
+    float leftShift;
+
+    //Define servo motor door positions
+    final double CLOSED_DOOR_POSITION = 0.4;
+    final double OPEN_DOOR_POSITION = 1.2;
+
+
+    //**********************************************************************************************************
+//METHODS BELOW
+    //Create a custom function to count the number of times the button "y" is pressed
+    //It toggles button y, so if it has been pressed an odd number of times, the door will be open
+    //Otherwise, it will be closed
+    public void toggleButtonY()
+    {
+        doorRight.setDirection(Servo.Direction.REVERSE);
+        yPressCount = yPressCount + 1;
+        if (yPressCount % 2 == 1) {
+
+            doorLeft.setPosition(OPEN_DOOR_POSITION);
+            doorRight.setPosition(OPEN_DOOR_POSITION);
+        }
+        else {
+
+            doorLeft.setPosition(CLOSED_DOOR_POSITION);
+            doorRight.setPosition(CLOSED_DOOR_POSITION);
+        }
+    }
+//Create a custom function to count the number of times the button "a" is pressed
+//It toggles button a, so if it has been pressed an odd number of times, the motor will go forward
+//Otherwise, it will be off
+>>>>>>> parent of d0a29b6... Patched
 
     /**
-     * public void toggleButtonA()
-     * {
+     * public void toggleButtonA() {
      * aPressCount = aPressCount + 1;
      * if (aPressCount % 2 == 1) {
      * elevator.setPower(1.0);
-     * }
-     * else
-     * {
+     * } else {
      * elevator.setPower(0.0);
      * }
      * }
@@ -172,8 +206,7 @@ public class remoteControlProgram extends SynchronousOpMode //CLASS START
 //        doorLeft = hardwareMap.servo.get("doorLeft");
 //        doorRight = hardwareMap.servo.get("doorRight");
 
-
-        //Reverse the left motors since it is facing the opposite direction as the left motor
+        //Reverse the right motors since it is facing the opposite direction as the left motor
         leftMotorFront.setDirection(DcMotor.Direction.REVERSE);
         leftMotorBack.setDirection(DcMotor.Direction.REVERSE);
 //***********************************************************************************************************
@@ -182,6 +215,7 @@ public class remoteControlProgram extends SynchronousOpMode //CLASS START
         //Open loops
         while (opModeIsActive()) {
             if (updateGamepads()) {
+<<<<<<< HEAD
                 //DRIVE MOTORS CODE
 
                 //Set float variables as the inputs from the joystick and the dpad
@@ -251,6 +285,78 @@ public class remoteControlProgram extends SynchronousOpMode //CLASS START
                     //Set the power of the football shooter so that it runs for the entire run
                     //shooterLeft.setPower(1.0);
                     //shooterRight.setPower(1.0);
+=======
+                //Set float variables as the inputs from the joystick and the dpad
+                //The negative sign is necessary because pushing the joystick up normally sends the robot backward
+                //Additionally, set float variables as the input from the triggers
+                driveY = -gamepad1.left_stick_y / (float) 2.5;
+                turnX = gamepad1.left_stick_x / (float) 2.5;
+                leftShift = gamepad1.left_trigger / (float) 2.5;
+                rightShift = gamepad1.right_trigger / (float) 2.5;
+
+
+                //Set the power of the motors with the joystick inputs
+                leftMotorFront.setPower(driveY);
+                leftMotorBack.setPower(driveY);
+                rightMotorFront.setPower(driveY);
+                rightMotorBack.setPower(driveY);
+
+
+                //Set up tank turning on the robot
+                if (turnX > 0) {
+                    leftMotorFront.setPower(-turnX);
+                    leftMotorBack.setPower(-turnX);
+                    rightMotorFront.setPower(turnX);
+                    rightMotorBack.setPower(turnX);
+                }
+                else if (turnX < 0) {
+                    rightMotorFront.setPower(turnX);
+                    rightMotorBack.setPower(turnX);
+                    leftMotorFront.setPower(-turnX);
+                    leftMotorBack.setPower(-turnX);
+                }
+                else { }
+
+
+                //Add left and right shift functionality
+                if (leftShift > 0) {
+                    leftMotorFront.setPower(leftShift);
+                    leftMotorBack.setPower(-leftShift);
+                    rightMotorFront.setPower(-leftShift);
+                    rightMotorBack.setPower(leftShift);
+                }
+                else if (rightShift > 0) {
+                    leftMotorFront.setPower(-rightShift);
+                    leftMotorBack.setPower(rightShift);
+                    rightMotorFront.setPower(rightShift);
+                    rightMotorBack.setPower(-rightShift);
+                }
+
+
+                //Attachments code
+
+                //Set the power of the spinner so that it runs for the entire run
+                //spinner.setPower(1.0);
+
+
+                //Set the position of the door in 2 different situations, using the "y" button
+                // The 2nd situation is void
+                if (gamepad2.y) {
+                    toggleButtonY();
+                } else { }
+
+                //Set the power of the elevator in 2 different situations, using the "a" button.
+                // The 2nd situation is void
+                //if (gamepad2.a) {
+                //  toggleButtonA();
+                //} else {
+                //}
+
+
+                //Set the power of the football shooter so that it runs for the entire run
+                //shooterLeft.setPower(1.0);
+                //shooterRight.setPower(1.0);
+>>>>>>> parent of d0a29b6... Patched
 
                 } //Close inside "if" loop
                 telemetry.update();
