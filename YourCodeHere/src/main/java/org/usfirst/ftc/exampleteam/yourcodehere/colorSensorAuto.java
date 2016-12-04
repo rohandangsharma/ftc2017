f3a38e601d60d3e05f68822970fb92f94a24e8ec
rohandangsharma <rohandangsharma@gmail.com>
2016-12-04 02:00:32 -0500

    Lots of Light Sensor Fixes (Don't Change)

---
+++ YourCodeHere/src/main/java/org/usfirst/ftc/exampleteam/yourcodehere/colorSensorAuto.java
@@ -0,0 +1,77 @@
+//This is the main autonomous program with encoders for FTC Velocity Vortex 2016
+//Authors: FTC team [11212??], The Lexington Legoheads
+//In case of questions email anupendra@gmail.com (coach)
+//**********************************************************************************************************
+//Run from the necessary package
+
+//Import necessary items
+package org.usfirst.ftc.exampleteam.yourcodehere;
+
+import android.app.Activity;
+import android.graphics.Color;
+import android.view.View;
+
+import com.qualcomm.ftcrobotcontroller.R;
+import com.qualcomm.robotcore.eventloop.opmode.Disabled;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
+import com.qualcomm.robotcore.hardware.ColorSensor;
+
+import org.swerverobotics.library.SynchronousOpMode;
+
+
+@Autonomous(name = "colorSensorAuto", group = "Sensor")
+public class colorSensorAuto extends SynchronousOpMode {
+
+    ColorSensor colorSensor;
+
+    @Override
+    public void main() throws InterruptedException {
+
+        // hsvValues is an array that will hold the hue, saturation, and value information.
+        float hsvValues[] = {0F, 0F, 0F};
+
+        // values is a reference to the hsvValues array.
+        final float values[] = hsvValues;
+
+        // get a reference to the RelativeLayout so we can change the background
+        // color of the Robot Controller app to match the hue detected by the RGB sensor.
+        final View relativeLayout = ((Activity) hardwareMap.appContext).findViewById(com.qualcomm.ftcrobotcontroller.R.id.RelativeLayout);
+
+        // get a reference to our ColorSensor object.
+        colorSensor = hardwareMap.colorSensor.get("colorSensor");
+
+
+        // wait for the start button to be pressed.
+        waitForStart();
+
+        while (opModeIsActive()) {
+            // Set the LED in the beginning
+            colorSensor.enableLed(true);
+
+            // while the op mode is active, loop and read the RGB data.
+            // convert the RGB values to HSV values.
+            Color.RGBToHSV(colorSensor.red() * 8, colorSensor.green() * 8, colorSensor.blue() * 8, hsvValues);
+
+            // send the info back to driver station using telemetry function.
+            telemetry.addData("Clear", colorSensor.alpha());
+            telemetry.addData("Red  ", colorSensor.red());
+            telemetry.addData("Green", colorSensor.green());
+            telemetry.addData("Blue ", colorSensor.blue());
+            telemetry.addData("Rohan", colorSensor.argb());
+            telemetry.addData("Hue", hsvValues[0]);
+
+            // change the background color to match the color detected by the RGB sensor.
+            // pass a reference to the hue, saturation, and value array as an argument
+            // to the HSVToColor method.
+            relativeLayout.post(new Runnable() {
+                public void run() {
+                    relativeLayout.setBackgroundColor(Color.HSVToColor(0x3C, values));
+                }
+            });
+            telemetry.update();
+            idle(); // Always call idle() at the bottom of your while(opModeIsActive()) loop
+        }
+    }
+}
